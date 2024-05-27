a402b6e888cea405bf76d486305526d9149098fa
Nicholas Choong <nicholascwc1997@gmail.com>
2024-05-28 03:33:28 +0800

    delete depulicates

--- util/clone.py
+++
@@ -1,6 +0,0 @@
-from copy import deepcopy
-from torch.nn import Module, ModuleList
-
-
-def get_clones(module: Module, N: int):
-    return ModuleList([deepcopy(module) for _ in range(N)])
--- util/parameter_count.py
+++
@@ -1,5 +0,0 @@
-from torch.nn import Module
-
-
-def count_parameters(model: Module):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
